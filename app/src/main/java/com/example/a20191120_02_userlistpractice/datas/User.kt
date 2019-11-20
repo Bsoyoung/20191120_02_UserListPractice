0389bfb3e09b05e585dc00b20f56640b2284e903
so_young <qksthdhd94@gmail.com>
2019-11-20 21:22:18 +0900

    리스트뷰 / 데이터 클래스 / 서버 API 생성

---
+++ app/src/main/java/com/example/a20191120_02_userlistpractice/datas/User.kt
@@ -0,0 +1,12 @@
+package com.example.a20191120_02_userlistpractice.datas
+
+class User {
+
+    var loginId = ""
+    var name=""
+    var phone=""
+
+    var category:Category? = null
+
+
+}
---
+++ app/src/main/java/com/example/a20191120_02_userlistpractice/utils/ConnectServer.kt
@@ -0,0 +1,50 @@
+package com.example.a20191120_02_userlistpractice.utils
+
+import android.content.Context
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class ConnectServer {
+
+    interface JsonResponseHandler{
+        fun onResponse(json:JSONObject)
+    }
+
+    companion object{
+        var BASE_URL = "http://192.168.0.26:5000"
+
+        fun getRequestUserList(context:Context, needActive:String,handler:JsonResponseHandler?){
+
+            var client = OkHttpClient()
+            var urlBuilder = HttpUrl.parse("${BASE_URL}/admin/user").newBuilder()
+
+            urlBuilder.addEncodedQueryParameter("active",needActive)
+
+            var requestUrl = urlBuilder.build().toString()
+
+            var request = Request.Builder()
+                .url(requestUrl)
+                //만약 header가 필요하면 header() 함수 사용.
+                .build()
+
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val body = response.body()!!.string()
+                    val jsonObject = JSONObject(body)
+                    handler?.onResponse(json)
+
+
+                }
+
+            })
+
+        }
+    }
+
+}
